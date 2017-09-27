439cae119527a06fb42f3f6245f54a87fad7d58e
e <614466874@qq.com>
2017-09-27 18:29:20 +0800

    产生数字的算法

--- app/src/main/java/com/zgmao/game/activity/AddNumberActivity.java
+++ app/src/main/java/com/zgmao/game/activity/AddNumberActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
+import com.maf.utils.Lg;
 import com.zgmao.game.R;
 import com.zgmao.game.adapter.AddNumberAdapter;
 import com.zgmao.game.bean.AddNumberItem;
@@ -33,7 +34,13 @@ public class AddNumberActivity extends TouchActivity
     private RecyclerView recycler_cube;
     // 最大数目
     private int maxNumber = 16;
+    // 行数
+    private int rows = 4;
+    //列数
+    private int column = 4;
+    // 数字列表
     private List<AddNumberItem> numberList;
+    // 数字列表设配器
     private AddNumberAdapter addNumberAdapter;
 
     @Override
@@ -67,7 +74,10 @@ public class AddNumberActivity extends TouchActivity
     @Override
     protected void initValue()
     {
-
+        // 初始出现两次数字
+        randomNumber(true);
+        randomNumber(true);
+        addNumberAdapter.notifyDataSetChanged();
     }
 
     @Override
@@ -107,18 +117,117 @@ public class AddNumberActivity extends TouchActivity
      * 3：
      *
      * @param isFirst 是否是第一次，如果是，产生两个
+     * @return 是否成功生成，如果没有，表示游戏结束
      */
-    private void randomNumber(boolean isFirst)
+    private boolean randomNumber(boolean isFirst)
     {
         NumberEnum randomNumber;
-        if (isRate(0.8)) {
+        if (isFirst) {
+            // 第一次，出现的是初始值
             randomNumber = NumberEnum.ONE;
         } else {
-            randomNumber = NumberEnum.TWO;
+            if (isRate(0.9)) {
+                randomNumber = NumberEnum.ONE;
+            } else {
+                randomNumber = NumberEnum.TWO;
+            }
         }
+        // 出现在哪个索引位置
+        int row = 0;
+        if (!isFirst) {
+            // 不是第一次，才随机，否则始终是第0行
+            row = randomRow();
+        }
+
+        Lg.d("出现在第" + row + "行");
+        if (row == -1) {
+            return false;
+        }
+        int column = randomColumn();
+        Lg.d("出现在第" + column + "列");
+        if (column == -1) {
+            return false;
+        }
+        int index = row * rows + column;
+        Lg.d("下次出现的位置是:" + index);
+        AddNumberItem addNumberItem = numberList.get(index);
+        addNumberItem.setNumberEnum(randomNumber);
+        return true;
     }
 
     /**
+     * 出现数字的概率，有null位的行越多，出现数字的概率越大
+     *
+     * @return
+     */
+    private int randomRow()
+    {
+        // 是否已满，如果发现空位置，设为false
+        boolean isFull = true;
+        // 第一次发现null单位的行，如果根据概率依然没有得到null行，则返回第一次发现null单位的行
+        int firstNullRow = 0;
+        int[][] lines = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+        for (int i = 0; i < lines.length; i++) {
+            int[] items = lines[i];
+            int nullNumber = 0;// 每行有多少个null
+            for (int j = 0; j < items.length; j++) {
+                // 每行的每个
+                int itemNumber = items[j];
+                if (numberList.get(itemNumber).getNumberEnum() == null) {
+                    nullNumber++;
+                    if (isFull) {
+                        // 第一次发现null行
+                        firstNullRow = i;
+                        isFull = false;
+                    }
+                }
+            }
+            // 出现数字的概率，有null位的行越多，出现数字的概率越大
+            float showNumberRate = (float) nullNumber / items.length;
+            Lg.d("第" + i + "行有" + nullNumber + "个空白区域，" + "概率是" + showNumberRate);
+            if (isRate(showNumberRate)) {
+                return i;
+            }
+        }
+        if (isFull) {
+            // 没有发现null行，直接返回-1
+            return -1;
+        }
+        // 发现null行，但是凭借概率没有得到行，默认返回第一次发现的null行
+        return firstNullRow;
+    }
+
+    /**
+     * 所有null列，每列出现的概率相同
+     *
+     * @return
+     */
+    private int randomColumn()
+    {
+        // 第一次发现null单位的列，如果根据概率依然没有得到null列，则返回第一次发现null单位的列
+        int[][] lines = {{0, 4, 8, 12}, {1, 5, 9, 13}, {2, 6, 10, 14}, {3, 7, 11, 15}};
+        List<Integer> nullColumnList = new ArrayList<>();
+        for (int i = 0; i < lines.length; i++) {
+            int[] items = lines[i];
+            for (int j = 0; j < items.length; j++) {
+                // 每列的每个
+                int itemNumber = items[j];
+                if (numberList.get(itemNumber).getNumberEnum() == null) {
+                    nullColumnList.add(i);
+                }
+            }
+        }
+        int size = nullColumnList.size();
+        if (size == 0) {
+            return -1;
+        }
+        double random = Math.random() * size;
+        return nullColumnList.get((int) random);
+    }
+
+    /**
+     * 按照比例，随机产生是/否
+     *
      * @param rate 比例0.0-1.0
      * @return
      */
